ce6c57d50c4fd067a1d5e0296dc24171004cde53
Yuri Luiz <yuri.luitzkt@gmail.com>
2024-07-22 12:08:25 -0300

    refactor: Update footer component

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -2,35 +2,38 @@ import { Github, HandCoins, Linkedin } from 'lucide-react'
 
 export const CopyrightFooter = () => {
     return (
-        <footer className="bg-gray-800 text-white py-2 mt-8 w-full">
+        <footer className="dark:text-white py-2 mt-8 w-full text-black">
             <div className="container mx-auto flex flex-col items-center justify-center">
                 <p className="text-sm mb-2">
                     © 2024 Focus Extension | Yuri Luiz
                 </p>
-                <div className="flex space-x-4">
+                <div className="flex space-x-4 text-xs ">
                     <a
                         href="https://github.com/yuuhLKT"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-white hover:text-gray-400"
+                        className="dark:text-white text-black hover:text-gray-400 flex flex-col items-center"
                     >
                         <Github size={20} />
+                        <div className="text-center mt-1">Github</div>
                     </a>
                     <a
                         href="https://www.linkedin.com/in/yurilkt"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-white hover:text-gray-400"
+                        className="dark:text-white text-black hover:text-gray-400 flex flex-col items-center"
                     >
                         <Linkedin size={20} />
+                        <div className="text-center mt-1">Linkedin</div>
                     </a>
                     <a
                         href="https://www.pixme.bio/yuuhlkt"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-white hover:text-gray-400"
+                        className="dark:text-white text-black hover:text-gray-400 flex flex-col items-center"
                     >
                         <HandCoins size={20} />
+                        <div className="text-center mt-1">Donate</div>
                     </a>
                 </div>
             </div>
